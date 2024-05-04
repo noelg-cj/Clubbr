9119ad5195a082a2ab0c4a87f1cbbf12b6efac4c
Akash <AwesomeAkash47@gmail.com>
2024-05-04 19:17:52 +0530

    events fetch in DisplayEvents

--- src/components/DisplayEvents.vue
+++ src/components/DisplayEvents.vue
@@ -1,6 +1,40 @@
-<script setup>
+<script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      eventList: [],
+    };
+  },
+  mounted() {
+    this.fetchEvents();
+  },
+  methods: {
+    fetchEvents() {
+      axios.get('http://localhost:5000/event_list')
+        .then(response => {
+          this.eventList = response.data;
+          console.log(this.eventList)
+        })
+        .catch(error => {
+          console.error('Error fetching posts:', error);
+        });
+    },
+    /*addPost() {
+      axios.post('http://localhost:3000/posts', this.newPost)
+        .then(() => {
+          this.newPost = { title: '', content: '' };
+          this.fetchPosts(); // Refresh the list after adding
+        })
+        .catch(error => {
+          console.error('Error adding post:', error);
+        });
+    }*/
+  }
+};
 </script>
+
 
 <template>
     <h1>The below are the event details</h1>
